aba34846df3d6b6201f54d6b9eb5f28d9018d007
Balakumaran <bkumaran121@gmail.com>
2023-09-26 20:44:11 +0530

    updated

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const AddToCart = () => {
-  return <button onClick={() => console.log("clicked")}>Add</button>;
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("clicked")}>
+      Add To Cart
+    </button>
+  );
 };
 
 export default AddToCart;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-interface User {
-  id: number;
-  name: string;
-}
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-  const users: User[] = await res.json();
+import UserTable from "./UserTable";
+const UsersPage = () => {
   return (
     <div>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <UserTable />
     </div>
   );
 };
