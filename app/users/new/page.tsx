6091f3db18a13faa0cb8a058888bc39b1ce56508
Balakumaran <bkumaran121@gmail.com>
2023-10-06 19:22:13 +0530

    updated

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const NewUserPage = () => {
+  const router = useRouter();
   return (
-    <div>NewUserPage</div>
-  )
-}
+    <div>
+      <button className="btn btn-primary" onClick={() => router.push("/users")}>
+        Create
+      </button>
+    </div>
+  );
+};
 
-export default NewUserPage
+export default NewUserPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 interface Props {
   searchParams: { sortOrder: string };
 }
 const UsersPage = ({ searchParams: { sortOrder } }: Props) => {
-  console.log(sortOrder, "so");
   return (
     <div>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </div>
   );
